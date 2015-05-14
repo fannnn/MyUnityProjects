45203448a189934f8647a72b1511095715b0c535
fannnn <fan@fingerrockz.com>
2015-05-14 14:32:29 +0800

    fdfs

---
+++ TESTtoCODE/Assets/Scripts/UselessShit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UselessShit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.right * 99*Time.deltaTime);
+		transform.Rotate(Vector3.up * 99*Time.deltaTime, Space.World);
+	}
+}
