69de8cc1fde1e052f2f3246d6520c9baddcdfa25
fannnn <fan@fingerrockz.com>
2015-05-14 12:56:23 +0800

    yyy

---
+++ LearnToCODE_alpha/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+	public int maxHealth = 100;
+	public int curHealth = 100;
+	public float healthBarLength ;
+
+	// Use this for initialization
+	void Start () {
+	//代入變數=healthBarLength = 螢幕寬度的一半
+		healthBarLength = Screen.width / 2;
+					}
+	// Update is called once per frame
+	void Update () {
+		AddjustCurrentHealth (0);
+			}
+	void OnGUI(){
+		GUI.Box (new Rect (10,40,healthBarLength,20), curHealth + "/" + maxHealth);
+							}
+		public void AddjustCurrentHealth(int adj){
+		//curHealth代入curHealth加上adj
+		curHealth += adj;
+
+		if (curHealth < 0)
+			curHealth = 0;
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+		if (maxHealth < 1)
+			maxHealth = 1;
+
+		//healthBarLenght = 螢幕的一半 * curHealth / (float???)maxHealth 
+		healthBarLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
+
+	}
+}
---
+++ LearnToCODE_alpha/Assets/Scripts/Targetting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Targetting : MonoBehaviour {
+	public List<Transform>targets;
+	// Use this for initialization
+	void Start () {
+		targets = new List<Transform>();
+	}
+	public void AddAllEnemies()
+	{
+		GameObject[] go = GameObject.FindGameObjectsWithTag ("Enemy");
+		//foreach(GameObject enemy in go)
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
